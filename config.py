34ed91a0a16189c2513f87100387e194be0c04b8
Gohil-Bhumika-4Born <gohilbhumika@4born.com>
2026-01-06 12:19:47 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = "dev-key-change-this"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
